04abd3894129f5a1b4157de49aa580db86461168
A-karkaih <akarkaih@gmail.com>
2026-04-29 18:19:27 +0100

    Position & Movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
-import "./App.css";
+import { motion } from "motion/react";
 export default function App() {
-  return <>Hello world</>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <motion.div
+        style={{ textAlign: "center" }}
+        initial={{
+          opacity: 0,
+          y: 300,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{ duration: 2, ease: "easeInOut" }}
+      >
+        <motion.h1>Hello world </motion.h1>
+      </motion.div>
+    </div>
+  );
 }
